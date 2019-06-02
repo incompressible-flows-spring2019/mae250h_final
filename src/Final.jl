b54080eb7983f5aa5f60051f132028bdfae983f9
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-06-02 16:09:32 -0700

    Initial commit

---
+++ src/Final.jl
@@ -0,0 +1,20 @@
+module Final
+
+include("timemarching.jl")
+include("trisolve.jl")
+include("datatypes.jl")
+include("operations.jl")
+include("grid.jl")
+include("parameters.jl")
+include("mapping.jl")
+include("bc.jl")
+
+include("pointdatatypes.jl")
+include("ibpm.jl")
+
+
+include("diffusion.jl")
+include("convection.jl")
+include("poisson.jl")
+
+end # module
